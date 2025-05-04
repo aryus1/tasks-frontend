c3b7bbf64af90cf833abddc413260188945b1157
Marcelo <marcelocoamae@gmail.com>
2025-05-04 14:48:36 -0300

    feat(dashboard todo): visual da tela de dashboard das listas to do

--- src/components/WorkspaceKanban.js
+++ src/components/WorkspaceKanban.js
@@ -1,7 +1,17 @@
+import { useState } from "react";
+import { FiLogIn } from "react-icons/fi";
+import { GoTrash } from "react-icons/go";
 import { IoIosAddCircleOutline, IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
+import { IoCloseCircleOutline } from "react-icons/io5";
 import { MdOutlineViewKanban } from "react-icons/md";
 
 export default function WorkspaceKanban() {
+    const [modalOpen, setModalOpen] = useState(false);
+
+    const handleModalOpen = () => {
+        setModalOpen(true);
+    }
+    
     return (
         <div className="p-4 md:p-8 lg:px-24 lg:py-12 flex flex-col gap-6">
             <div className="bg-[#EAF7D5] p-6 rounded-xl text-gray-600 text-sm md:text-base">
@@ -52,13 +62,78 @@ export default function WorkspaceKanban() {
             </div>
 
             <div className="flex justify-center items-center">
-                <button className="flex items-center justify-center gap-4 bg-[#90E528] hover:bg-[#7cc91f] font-semibold px-6 py-4 rounded-xl border border-black border-b-4 mt-16">
+                <button onClick={handleModalOpen} className="flex items-center justify-center gap-4 bg-[#90E528] hover:bg-[#7cc91f] font-semibold px-6 py-4 rounded-xl border border-black border-b-4 mt-16">
                     <div className="text-2xl">
                         <IoIosAddCircleOutline />
                     </div>
                     Criar novo quadro
                 </button>
             </div>
+
+            {modalOpen && (
+                <div className="fixed inset-0 flex items-center justify-center flex-col bg-black bg-opacity-50">
+
+                    <div className="border-2 border-[#90E528] rounded-2xl">
+                        <div className="flex gap-4 items-center justify-between bg-[#90E528] p-4 rounded-t-xl">
+                            <h2 className="text-xl font-semibold">Seu novo quadro</h2>
+                            <button onClick={() => setModalOpen(false)} className="text-2xl">
+                                <IoCloseCircleOutline />
+                            </button>
+                        </div>
+                        <div className="bg-white p-4 rounded-b-xl shadow-lg w-full max-w-md">
+                            <form>
+                                <div className="flex items-center gap-4">
+                                    <p className="text-lg font-semibold">Título:</p>
+                                    <input type="text" placeholder="Criar título" className="p-2 rounded" />
+                                </div>
+
+                                <div className="flex items-center gap-4">
+                                    <p className="text-lg font-semibold">Data de vencimento:</p>
+                                    <input type="text" placeholder="Nenhuma" className="p-2 rounded" />
+                                </div>
+
+                                <div className="flex items-center gap-6">
+                                    <p className="text-lg font-semibold">Categoria:</p>
+                                    <input type="text" placeholder="Criar categoria" className="p-2 rounded" />
+                                </div>
+
+                                <div className="flex items-center gap-6">
+                                    <p className="text-lg font-semibold">Cor do quadro:</p>
+                                    <div className="flex gap-4 mt-2">
+                                        <div className="bg-[#74CF4A] w-8 h-8 rounded-full"></div>
+                                        <div className="bg-[#FFC91A] w-8 h-8 rounded-full"></div>
+                                        <div className="bg-[#DD4A4A] w-8 h-8 rounded-full"></div>
+                                        <div className="bg-[#CB298A] w-8 h-8 rounded-full"></div>
+                                    </div>
+                                </div>
+                            </form>
+                            <hr className="mt-8"></hr>
+
+                            <div className="flex justify-between items-center mt-4">
+                                <button onClick={() => setModalOpen(false)}>
+                                    <div className="flex items-center gap-4 bg-[#DF2D2D]/15 text-[#DF2D2D] font-semibold px-6 py-2 rounded-2xl mt-8">
+                                        <div className="font-bold text-xl">
+                                            <GoTrash />
+                                        </div>
+                                        <p>Cancelar</p>
+                                    </div>
+                                </button>
+
+                                <button>
+                                    <div>
+                                        <div className="flex items-center gap-4 bg-[#CFF5A4] text-[#3D5536] font-semibold px-6 py-2 rounded-2xl mt-8">
+                                            <div className="font-bold text-xl">
+                                                <FiLogIn />
+                                            </div>
+                                            <p>Criar quadro</p>
+                                        </div>
+                                    </div>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
