4f2e1978f0b9b8b68db0ffe56a4b3b08f92ab2cc
victor linhares <victorlinharesdev@gmail.com>
2025-05-11 22:51:00 -0300

    criação de icones e ajuste de espaçamentos

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import { MdOutlineEmail } from "react-icons/md";
-import { IoIosArrowForward } from "react-icons/io";
+import { Icons } from "src/components/Icons";
 import { Link, useNavigate } from "react-router-dom";
 
 export default function Login() {
@@ -19,18 +18,19 @@ export default function Login() {
     };
 
     return (
-        <div>
-            <div className="flex justify-center items-center mt-10">
+        <main className="flex justify-center items-center min-h-screen">
+        <div> 
+            <div className="flex justify-center">
                 <img src="./assets/logo.png" alt="Logo Dasky" />
             </div>
 
-            <section className="flex flex-col justify-center items-center mt-20 space-y-6">
+            <section className="flex flex-col justify-center items-center">
                 <h1 className="font-bold text-2xl text-[#203309] text-center">
-                    Digite seu e-mail <br /> para entrar
+                    Digite seu e-mail <span className="block">para entrar</span>
                 </h1>
 
                 <div className="relative max-w-sm flex items-center">
-                    <MdOutlineEmail className="absolute left-4 top-1/2 transform -translate-y-1/2 text-gray-500" />
+                    <Icons.Mail className="h-6 w-6 text-gray-500" />
                     <input
                         type="email"
                         className="w-full pl-12 pr-4 py-2 border-2 border-[#C1CBB5] rounded-xl"
@@ -43,21 +43,18 @@ export default function Login() {
                 </div>
 
                 <div
-                    className="flex items-center w-fit px-16 py-2 bg-[#A0FF2C] border-b-2 border-b-black rounded-xl cursor-pointer hover:scale-110 transition"
+                    className="flex items-center w-fit px-16 py-2 bg-[#A0FF2C] border-b-2 border-b-black rounded-xl cursor-pointer "
                     onClick={handleNavigate}
                 >
                     <button className="text-[#203309] font-bold">Enviar código</button>
-                    <IoIosArrowForward />
+                    <Icons.Mail className="h-6 w-6 text-gray-500" />
                 </div>
 
                 <p className="text-[#3D5536]">
                     Não tem uma conta? <strong><Link to={'/register'}>Crie agora!</Link></strong>
                 </p>
             </section>
-
-            <div className="mt-40 md:hidden">
-                <img src="./assets/group.png" alt="decor" />
-            </div>
         </div>
+        </main>
     );
 }
